374ff3434c1fd0817d297150f2a44aeb891f2a16
Mark Zheng <40537984+PopMark-Zheng@users.noreply.github.com>
2020-02-23 21:09:59 +0800

    Add files via upload

---
+++ Arknights-for-bilibili-ver.js
@@ -0,0 +1,9 @@
+auto.waitFor()
+toast ("将于程序启动10秒后启动")/*提示用户*/
+waitForPackage("com.hypergryph.arknights.bilibili")/*等待b版明日方舟启动*/
+sleep (10000)
+click (1766,993)/*第一次点击*/
+sleep (2000)
+click (1689,720)/*第二次点击*/
+sleep (120000)
+click (1766,291)/*退出自律*/
